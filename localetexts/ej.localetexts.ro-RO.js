fa5bce2cc9707204f428d0488222308728227ce4
KarthickNR <jumbojenis@gmail.com>
2016-03-17 14:09:46 +0530

    version removed and copyrights updated
    version removed and copyrights updated and committed

--- localetexts/ej.localetexts.ro-RO.js
+++ localetexts/ej.localetexts.ro-RO.js
@@ -1,7 +1,6 @@
 /*!
 *  filename: ej.localetexts.ro-RO.js
-*  version : 13.4.0.58
-*  Copyright Syncfusion Inc. 2001 - 2014. All rights reserved.
+*  Copyright Syncfusion Inc. 2001 - 2016. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
 *  licensing@syncfusion.com. Any infringement will be prosecuted under
